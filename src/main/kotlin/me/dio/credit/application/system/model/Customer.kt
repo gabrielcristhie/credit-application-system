3197ee930b0a7de148e1695dde3efe617452c003
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2024-01-12 23:05:29 -0300

    Atualizando entidade

--- src/main/kotlin/me/dio/credit/application/system/model/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/model/Customer.kt
@@ -5,6 +5,7 @@ import java.math.BigDecimal
 
 @Entity
 data class Customer(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
         @Column(nullable = false) var firstName: String = "",
         @Column(nullable = false) var lastName: String = "",
         @Column(nullable = false, unique = true) var cpf: String = "",
@@ -14,5 +15,4 @@ data class Customer(
         @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
         @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
         cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
-        mappedBy = "customer") var credits: List<Credit> = mutableListOf(),
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null)
+        mappedBy = "customer") var credits: List<Credit> = mutableListOf())
